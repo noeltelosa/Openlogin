be2ad49af087f8c8c97854aeede98c7bfab15852
ntelosa <noeltelosa@gmail.com>
2019-05-31 22:26:45 +0800

    Revert "previous build"
    This reverts commit ecb9400816efcae71e298ed70d9c2edbf1021530.

--- Openlogin/Models/ChangePassword.cs
+++ Openlogin/Models/ChangePassword.cs
@@ -9,16 +9,20 @@ namespace Openlogin.Models
     public class ChangePassword
     {
         //[Required(ErrorMessage = "Username is required")]
+        [Display(Name ="Username")]
         public String Username { get; set; }
 
+        [Display(Name = "Old Password")]
         [Required(ErrorMessage = "Old Password is required")]
         [DataType(DataType.Password)]
         public String OldPassword { get; set; }
 
+        [Display(Name = "New Password")]
         [Required(ErrorMessage = "New Password is required")]
         [DataType(DataType.Password)]
         public String NewPassword { get; set; }
 
+        [Display(Name = "Confirm New Password")]
         [Compare("NewPassword", ErrorMessage = "Please confirm your password")]
         [DataType(DataType.Password)]
         public String ConfirmPassword { get; set; }
